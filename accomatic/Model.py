33fb41f99d2585af5c63e7df821384ad3a319b51
Hannah Macdonell <hannah.macdonell1@gmail.com>
2022-02-08 15:33:13 -0500

    Model updated to include attribute 'type'

--- accomatic/Model.py
+++ accomatic/Model.py
@@ -3,10 +3,12 @@ from accomatic.DataFileReader import DataFileReader
 class Model(DataFileReader):
 
     stats: dict  # Individual results ultimately stored here
+    type: str    # Differentiate between model and obs
 
     def __init__(self, file_path=''):
         super().__init__(file_path)
         self.stats = {}
+        self.type = 'Model'
 
     def get_stats(self) -> dict:
         return self.stats
@@ -14,3 +16,5 @@ class Model(DataFileReader):
     def set_stats(self, stat_dictionary: dict) -> None:
         self.stats = stat_dictionary
 
+    def get_type(self) -> str:
+        return self.type
