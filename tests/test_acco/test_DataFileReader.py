e8a6a5abe55ffb71c824d273ab334e433d4dd995
Hannah Macdonell <hannah.macdonell1@gmail.com>
2022-02-08 15:07:50 -0500

    DataFileReader stats field removed from class and test

--- tests/test_acco/test_DataFileReader.py
+++ tests/test_acco/test_DataFileReader.py
@@ -4,7 +4,7 @@ from accomatic.DataFileReader import *
 def test_DataFileReader():
     a = DataFileReader('tests/test_data/NGO-DD-1004_ST01.csv')
     assert a.get_name() == 'NGO-DD-1004_ST01'
-    assert bool(a.get_stats()) == False
+    assert a.get_file_path() == 'tests/test_data/NGO-DD-1004_ST01.csv'
     assert a.get_time_extent() == ['2016-01-01 00:00:00', '2016-12-31 21:00:00']
     pass
 
