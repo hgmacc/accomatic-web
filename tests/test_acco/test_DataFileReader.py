dee2d5603a553bbfd712ebc97976d2f9ccc547fd
Hannah Macdonell <hannah.macdonell1@gmail.com>
2022-02-09 10:14:27 -0500

    Updated test_csv name from NGO.. to test_csv

--- tests/test_acco/test_DataFileReader.py
+++ tests/test_acco/test_DataFileReader.py
@@ -2,9 +2,9 @@ from accomatic.DataFileReader import *
 
 
 def test_DataFileReader():
-    a = DataFileReader('tests/test_data/NGO-DD-1004_ST01.csv')
-    assert a.get_name() == 'NGO-DD-1004_ST01'
-    assert a.get_file_path() == 'tests/test_data/NGO-DD-1004_ST01.csv'
+    a = DataFileReader('tests/test_data/test_csv_data.csv')
+    assert a.get_name() == 'test_csv_data'
+    assert a.get_file_path() == 'tests/test_data/test_csv_data.csv'
     assert a.get_time_extent() == ['2016-01-01 00:00:00', '2016-12-31 21:00:00']
     pass
 
--- tests/test_acco/test_Model.py
+++ tests/test_acco/test_Model.py
@@ -2,9 +2,9 @@ from accomatic.Model import *
 
 
 def test_Model():
-    a = Model('tests/test_data/NGO-DD-1004_ST01.csv')
-    assert a.get_name() == 'NGO-DD-1004_ST01'
-    assert a.get_file_path() == 'tests/test_data/NGO-DD-1004_ST01.csv'
+    a = Model('tests/test_data/test_csv_data.csv')
+    assert a.get_name() == 'test_csv_data'
+    assert a.get_file_path() == 'tests/test_data/test_csv_data.csv'
     assert bool(a.get_stats()) == False
     assert a.get_time_extent() == ['2016-01-01 00:00:00', '2016-12-31 21:00:00']
     pass
