78957dfcd305a20107d7d980cfa0f844e948f454
Hannah Macdonell <hannah.macdonell1@gmail.com>
2022-02-23 17:25:36 -0500

    Remove dead code and redundant static typing

--- accomatic/Data.py
+++ accomatic/Data.py
@@ -28,22 +28,3 @@ class Data:
     def count(self) -> int:
         return self._model_count
 
-
-"""
-
-Clip all model output to obs dataset.
-
-:return: overlap: dict = {'beg': datetime, 'end' : datetime}
-overlap = self._obs.time_extent
-        
-    @property
-    def date_overlap(self) -> List[str]:
-        for mod in self._models:
-            if mod.time_extent['beg'] >= self._obs.time_extent['beg']:
-                overlap['beg'] = mod.time_extent['beg']
-
-            if mod.time_extent['end'] <= self._obs.time_extent['end']:
-                overlap['end'] = mod.time_extent['end']
-
-        return overlap
-"""
--- accomatic/Experiment.py
+++ accomatic/Experiment.py
@@ -8,7 +8,7 @@ class Experiment:
     _sett: Settings
     _sites: Sites
 
-    def __init__(self, data: Data, sett: Settings, sites: Sites):
+    def __init__(self, data, sett, sites):
         self._data = data
         self._sett = sett
         self._sites = sites
@@ -36,21 +36,3 @@ class Experiment:
             # Runs terrain test
             pass
 
-
-"""
-
-    # holds models
-    # and outputs
-    # can generate output dictionaries and put stats into models
-
-for m in all_accordance_funcs:
-    model_means[m] = all_accordance_results[m].mean()
-    # print("Means:\n",all_accordance_results[m].mean())
-    if m in ['RMSE', 'MAE']:
-        model_ranks[m] = all_accordance_results[m].mean().rank(axis=0, method='average', ascending=True)
-    else:
-        model_ranks[m] = all_accordance_results[m].mean().rank(axis=0, method='average', ascending=False)
-    accordance_max[m] = all_accordance_results[m].max()
-    accordance_min[m] = all_accordance_results[m].min()
-
-"""
