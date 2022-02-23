9566d3ff666e6787fc7af3c5e9e69bbea98a39f2
Hannah Macdonell <hannah.macdonell1@gmail.com>
2022-02-23 17:26:51 -0500

    set_toml and sit_csv renamed to settings_toml an sites_csv

--- tests/test_acco/test_accomatic.py
+++ tests/test_acco/test_accomatic.py
@@ -4,8 +4,8 @@ from accomatic.accomatic import *
 def test_accomatic():
     model_dir: str = "tests/test_data/test_mod_dir"
     obs_pth: str = "tests/test_data/test_obs_dir/test_obs.pickle"
-    set_toml: str = "tests/test_data/test_toml_settings.toml"
-    sit_csv: str = "tests/test_data/test_sites_list.csv"
+    settings_toml: str = "tests/test_data/test_toml_settings.toml"
+    sites_csv: str = "tests/test_data/test_sites_list.csv"
 
-    assert accomatic(model_dir, obs_pth, set_toml, sit_csv) == 1
+    assert accomatic(model_dir, obs_pth, settings_toml, sites_csv) == 1
     pass
