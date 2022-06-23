24ee2cd39e6d8b70acc06f790762f705d1fb8c60
Hannah Macdonell <hannah.macdonell1@gmail.com>
2022-06-23 13:01:43 -0400

    Project building

---
+++ README.md
@@ -0,0 +1,19 @@
+    if input() != 1: sys.exit(0)
+
+Geotop takes a normal date
+YY/MM/DD HH:MM
+Origin is = 1970 (unix time)
+ERDAPP format
+
+How to get sites from ERDAPP
+Only has Ekati data
+Download some Ekati data from erddap and see if the .nc structures play well with GTPEM
+
+Database has tables for "field" or "column" sort and filter super fast
+Index the last recorded time stamp
+
+50 cm
+Take blue from .5
+Time-slice ID: years, seasons, monthly, snow binary, entire dataset("all data"), "all_data_winter"
+Pre-configure
+
---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version = 1.0
+
+setuptools.setup(
+    name="accomatic",
+    version=version,
+    author="Hannah Macdonell",
+    author_email="hannah.macdonell@carleton.ca",
+    description="Running analysis on point-scale model simulations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hgmacc/accomatic-web",
+    packages=['accomatic'],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Scientific/Engineering :: Atmospheric Science"
+        ],
+    entry_points={
+        'console_scripts': [
+            'accomatic = accomatic-web.accomatic:main',
+        ]},
+
+    install_requires=['numpy',
+                      'pandas',
+                      'netCDF4',
+                      'scipy',
+                      'pydap',
+                      'tomlkit']
+)
